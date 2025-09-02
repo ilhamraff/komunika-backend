7c97449c8b3a3210997b0b6da5b7f27b79fed7a0
ilhamraff <ifadhilah261@gmail.com>
2025-09-02 20:01:12 +0700

    fix: update group member selection in getDiscoverGroups function

--- src/repositories/groupRepositories.ts
+++ src/repositories/groupRepositories.ts
@@ -44,7 +44,7 @@ export const getDiscoverGroups = async (name = "") => {
         select: {
           _count: {
             select: {
-              RoomMessage: true,
+              RoomMember: true,
             },
           },
         },
